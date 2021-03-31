513f01067e36f54246c5ca85525c1b02ba618f44
Nick Joseph <niickyymjj@gmail.com>
2021-03-30 22:32:33 -0400

    Points A, B et C complet

--- src/services/config.js
+++ src/services/config.js
@@ -5,7 +5,13 @@
 
 // Configuration Firebase (mettez-y les vôtres !)
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyB59hy-uKr1sfRHsEoC2GFPFfboh59chu4",
+  authDomain: "utilisateurs-ex4-3b8e2.firebaseapp.com",
+  projectId: "utilisateurs-ex4-3b8e2",
+  storageBucket: "utilisateurs-ex4-3b8e2.appspot.com",
+  messagingSenderId: "909059696592",
+  appId: "1:909059696592:web:9f1e346a83f358f63a3bd5",
+  measurementId: "G-06BPVTNQF9"
 };
 export default firebaseConfig;
 
